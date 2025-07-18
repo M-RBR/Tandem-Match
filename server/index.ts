a2c3eeebb0d69023ad6051a4100764471b16a168
maximrubenbenjamin <roozenma@gmail.com>
2025-07-18 17:22:50 +0200

    Set up server/client continued; user register; encrypted added

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+/*
+
 import express from "express";
 import cors from "cors";
 import userRouter from "./routes/users";
@@ -17,6 +19,63 @@ app.use(
   })
 );
 app.use(cors());
+
+app.post("/testing", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/users", userRouter);
+
+app.use("/*splat", (req, res) =>
+  res.status(404).json({ error: "Endpoint not found." })
+);
+
+if (!process.env.MONGO_URI) {
+  throw new Error("no mongo uri");
+}
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(
+        "Connection to MongoDB established, and server is running on http://localhost: " +
+          port
+      );
+    });
+  })
+  .catch((err) => console.log(err));
+
+  */
+
+import express from "express";
+import cors from "cors";
+import userRouter from "./routes/users";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+console.log(process.env.MAXIM);
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// >>>>>>>>>>> THIS IS THE CRITICAL CHANGE YOU NEED TO MAKE <<<<<<<<<<<
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Explicitly allow your client's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // If you plan to send cookies or authorization headers
+  })
+);
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 app.post("/testing", (req, res) => {
   res.send("Hello World!");
--- server/models/users.ts
+++ server/models/users.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    username: { type: String, default: "Anon" }, // user can exist without a username, maybe change later, sync with frontend
+    username: { type: String, default: "Anon" }, // user can exist without a username; maybe change later, sync with frontend
     password: { type: String, required: true },
   },
   { timestamps: true, collection: "users" }
